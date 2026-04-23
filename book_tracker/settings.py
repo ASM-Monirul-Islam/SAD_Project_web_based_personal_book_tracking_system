b52cdf2c3e24f8d4b86a9cb922ae0e4198a4bd90
A S M Monirul Islam <monirul.01401411046@gmail.com>
2026-04-24 01:38:43 +0600

    Update settings.py

--- book_tracker/settings.py
+++ book_tracker/settings.py
@@ -134,4 +134,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hafizasartanddecor@gmail.com'
-EMAIL_HOST_PASSWORD = 'mllm unel ugnk xmhq'
+EMAIL_HOST_PASSWORD = 'mllm unel **** ****'
